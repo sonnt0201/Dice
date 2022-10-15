f185b2a78d28cd315bf390a3bf55fe26c589b1d0
MTD-SONNT290\Dell <nguyenthaison0201@gmail.com>
2022-10-16 00:24:59 +0700

    delete all js code, rebuild in MVC Pattern

--- js/index.js
+++ js/index.js
@@ -1,105 +1,37 @@
-var score = [];
-var playerName = ["Player 1", "Player 2"];
-const nameTextFields = document.getElementsByClassName("player-name");
-for (element of nameTextFields) {
-  element.addEventListener("change", inputCheck);
-}
-
-function inputCheck() {
-  for (let index = 0; index < nameTextFields.length; index++) {
-    playerName[index] =
-      nameTextFields[index].value.length > 0
-        ? nameTextFields[index].value
-        : `Player ${index + 1}`;
-  }
-}
-// When click Start Button
-function startGame() {
-  for (let i = 0; i < 10; i++) {
-    dicing(i);
-  }
-}
-
-function dicing(times) {
-  setTimeout(function () {
-    // Dicing for each dice
-    for (let playerIndex = 0; playerIndex <= 1; playerIndex++) {
-      let value = Math.floor(Math.random() * 6);
-      setValue(value, playerIndex);
-      score[playerIndex] = value;
+class Model {
+  
+  constructor(numberOfDices){
+    this.numberOfDices = numberOfDices;
+    this.dices = [];
+    for (let i=0; i<this.numberOfDices; i++){
+      this.dices.push({
+        id: i,
+        value: 1,
+        valueToString : function() {
+          switch(this.value){
+            case 1: return 'one'; break;
+            case 2: return 'two'; break;  
+            case 3: return 'three'; break;
+            case 4: return 'four'; break;
+            case 5: return 'five'; break;
+            case 6: return 'six'; break;
+            default: return 'unknown'; break;
+          }
+          
+        }
+      })
     }
+  };
 
-    // Dicing done
-    if (times == 9) {
-      if (score[0] > score[1]) {
-        document.getElementsByClassName(
-          "winner-announcement"
-        )[0].innerHTML = `The Winner is ${playerName[0]}`;
-      } else if (score[0] < score[1]) {
-        document.getElementsByClassName(
-          "winner-announcement"
-        )[0].innerHTML = `The winner is ${playerName[1]}`;
-      } else {
-        document.getElementsByClassName("winner-announcement")[0].innerHTML =
-          "Two players draw";
-      }
-    }
-  }, times * 100);
+}
+class View {
+  constructor(){  
+  };
 }
 
-// Set value for dices
-function setValue(value, playerIndex) {
-  // console.log(value);
-  let stringValue;
-  switch (value) {
-    case 0:
-      stringValue = "one";
-      break;
-    case 1:
-      stringValue = "two";
-      break;
-    case 2:
-      stringValue = "three";
-      break;
-    case 3:
-      stringValue = "four";
-      break;
-    case 4:
-      stringValue = "five";
-      break;
-    case 5:
-      stringValue = "six";
-      break;
-    default:
-      stringValue = "one";
-      break;
+class Controller {
+  constructor(model, view){
+    this.model = model;
+    this.view = view;
   }
-  document.getElementsByClassName("dice")[
-    playerIndex
-  ].className = `fa-solid fa-dice-${stringValue} dice`;
-}
-
-function reset() {
-  for (let index = 0; index < playerName.length; index++) {
-    setValue(0, index);
-  }
-  document.getElementsByClassName("winner-announcement")[0].innerHTML =
-    "The winner is";
-}
-
-function addDice() {
-  let diceGrid = document.getElementById("dice-grid");
-  let diceMember = document.getElementsByClassName("dice-member")[0];
-  diceGrid.innerHTML += `
-  <div class="col-sm-6 dice-member">
-    <p style="text-align: center;">
-      <input type="text" class="player-name" maxlength="15" placeholder="Player 1">
-      <i class="fa-solid fa-dice-one dice"></i>
-  </p>
-  </div>
-  `;
-  console.log(diceGrid);
-}
-function onDevelopingAlert() {
-  alert("Chưa làm xong tính năng này bạn ôi ==.==");
 }
---
+++ js/oldCode.js
@@ -0,0 +1,107 @@
+var score = [];
+var playerName = ["Player 1", "Player 2"];
+const nameTextFields = document.getElementsByClassName("player-name");
+for (element of nameTextFields) {
+  element.addEventListener("change", inputCheck);
+}
+
+function inputCheck() {
+  for (let index = 0; index < nameTextFields.length; index++) {
+    playerName[index] =
+      nameTextFields[index].value.length > 0
+        ? nameTextFields[index].value
+        : `Player ${index + 1}`;
+  }
+}
+// When click Start Button
+function startGame() {
+  for (let i = 0; i < 10; i++) {
+    dicing(i);
+  }
+}
+
+function dicing(times) {
+  setTimeout(function () {
+    // Dicing for each dice
+    for (let playerIndex = 0; playerIndex <= 1; playerIndex++) {
+      let value = Math.floor(Math.random() * 6);
+      setValue(value, playerIndex);
+      score[playerIndex] = value;
+    }
+
+    // Dicing done
+    if (times == 9) {
+      if (score[0] > score[1]) {
+        document.getElementsByClassName(
+          "winner-announcement"
+        )[0].innerHTML = `The Winner is ${playerName[0]}`;
+      } else if (score[0] < score[1]) {
+        document.getElementsByClassName(
+          "winner-announcement"
+        )[0].innerHTML = `The winner is ${playerName[1]}`;
+      } else {
+        document.getElementsByClassName("winner-announcement")[0].innerHTML =
+          "Two players draw";
+      }
+    }
+  }, times * 100);
+}
+
+// Set value for dices
+function setValue(value, playerIndex) {
+  // console.log(value);
+  let stringValue;
+  switch (value) {
+    case 0:
+      stringValue = "one";
+      break;
+    case 1:
+      stringValue = "two";
+      break;
+    case 2:
+      stringValue = "three";
+      break;
+    case 3:
+      stringValue = "four";
+      break;
+    case 4:
+      stringValue = "five";
+      break;
+    case 5:
+      stringValue = "six";
+      break;
+    default:
+      stringValue = "one";
+      break;
+  }
+  document.getElementsByClassName("dice")[
+    playerIndex
+  ].className = `fa-solid fa-dice-${stringValue} dice`;
+}
+
+// Reset the dices
+function reset() {
+  for (let index = 0; index < playerName.length; index++) {
+    setValue(0, index);
+  }
+  document.getElementsByClassName("winner-announcement")[0].innerHTML =
+    "The winner is";
+}
+
+// add dices
+function addDice() {
+  let diceGrid = document.getElementById("dice-grid");
+  let diceMember = document.getElementsByClassName("dice-member")[0];
+  diceGrid.innerHTML += `
+  <div class="col-sm-6 dice-member">
+    <p style="text-align: center;">
+      <input type="text" class="player-name" maxlength="15" placeholder="Player 1">
+      <i class="fa-solid fa-dice-one dice"></i>
+  </p>
+  </div>
+  `;
+  console.log(diceGrid);
+}
+function onDevelopingAlert() {
+  alert("Chưa làm xong tính năng này bạn ôi ==.==");
+}
