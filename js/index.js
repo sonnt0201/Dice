67d7a48d0c6fba498c724985ea65129eed9a59e9
MTD-SONNT290\Dell <nguyenthaison0201@gmail.com>
2022-10-29 15:26:10 +0700

    complete add a dice and remove a dice

--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
-var numberOfDices = 2;
+var dices = [
+    {name: 'Player 1', val: 0},
+    {name: 'Player 2', val: 0},
+]
+function layoutForGrid(number){
+    console.log($('.dice-member')[0])
+    for (const element of $('.dice-member')) {
+        element.className = `col-md-${(number % 3 == 0 || number % 3 == 2)? 4 : 6} dice-member`;
+        console.log(element);
+    };
+}
 $(document).ready(function(){
-    
+
+    // Add a dice
+    $('#add-dice').click(function(){
+        if (dices.length >= 10 ) return;
+        dices.push({name: `Player ${dices.length + 1}`, val: 1});
+        $('#dice-grid').append(
+            `<div class="col-md-6 dice-member">
+                <p style="text-align: center;">
+                    <input type="text" class="player-name" maxlength="15" placeholder="Player ${dices.length}">  <br>
+                    <i class="fa-solid fa-dice-one dice"></i>
+                </p>
+            </div>`
+        );
+        layoutForGrid(dices.length);
+    })
+
+    // Remove a dice
+    $('#remove-dice').click(function(){
+        if (dices.length == 2 ) return;
+        $('.dice-member')[dices.length - 1].remove();
+        dices.pop();
+        layoutForGrid(dices.length);
+    })
+
+    // TODO: - History on 
+
+    // 
 })
